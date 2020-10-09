f06ac272204cec0eb9c0dd22b47cb8227f747df5
searene <mycityofsky@gmail.com>
2020-10-09 08:47:35 +0800

    save before switching branches

--- src/main/infrastructure/repository/ConfigRepository.ts
+++ src/main/infrastructure/repository/ConfigRepository.ts
@@ -1,4 +1,6 @@
 import { BaseRepository } from "./BaseRepository";
+import { ConfigQuery } from "../query/ConfigQuery";
+import { ConfigDO } from "../do/ConfigDO";
 
 export interface ConfigRepository
   extends BaseRepository<ConfigQuery, ConfigDO> {}
