e1bac6469cc899baa526bc23b82046bdbe88d86a
searene <mycityofsky@gmail.com>
2020-10-31 11:21:26 +0800

    use ReactHook in Library

--- src/renderer/components/Library.tsx
+++ src/renderer/components/Library.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { useState, useEffect } from "react";
 import {
   Header,
   Container,
@@ -13,157 +14,131 @@ import { AddBookModal } from "./AddBookModal";
 import { BookVO } from "../../main/domain/BookVO";
 import serviceProvider from "../ServiceProvider";
 import { RouteComponentProps } from "react-router";
+import { Router } from "../route/Router";
 
 interface LibraryProps extends RouteComponentProps {}
 
-interface LibraryStates {
-  books: BookVO[];
-  initiated: boolean;
-  showAddBookModal: boolean;
-  showRemoveBookModal: boolean;
-}
+export function Library(props: LibraryProps) {
+  const [books, setBooks] = useState<BookVO[]>([]);
+  const [initiated, setInitiated] = useState(false);
+  const [showAddBookModal, setShowAddBookModal] = useState(false);
+  const [showRemoveBookModal, setShowRemoveBookModal] = useState(false);
 
-export class Library extends React.Component<LibraryProps, LibraryStates> {
-  constructor(props: LibraryProps) {
-    super(props);
-    this.state = {
-      books: [],
-      initiated: false,
-      showAddBookModal: false,
-      showRemoveBookModal: false,
-    };
-  }
-
-  async componentDidMount() {
-    await this.init();
-  }
-
-  render() {
-    if (!this.state.initiated) {
-      return <></>;
+  useEffect(() => {
+    if (!initiated) {
+      init();
     }
-    return (
-      <Container>
-        <Grid columns={3}>
-          <Grid.Row>
-            <Grid.Column width={8}>
-              <Header>Books</Header>
-            </Grid.Column>
-            <Grid.Column textAlign={"right"} width={8}>
-              <AddBookModal
-                visible={this.state.showAddBookModal}
-                onOpen={() => this.setState({ showAddBookModal: true })}
-                onClose={this.handleCloseOnAddBookModal}
-                trigger={
-                  <Button icon labelPosition={"left"}>
-                    <Icon name={"add"} /> Add
-                  </Button>
-                }
-              />
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-        <Divider />
-        <Table basic="very" selectable>
-          <Table.Header>
-            <Table.Row>
-              <Table.HeaderCell>Name</Table.HeaderCell>
-              <Table.HeaderCell>Total Word Count</Table.HeaderCell>
-            </Table.Row>
-          </Table.Header>
-          <Table.Body>
-            {this.state.books.map(book => (
-              <Table.Row key={book.id}>
-                <Table.Cell
-                  onClick={() => this.handleClickOnBook(book)}
-                  className={"hover-link"}
-                >
-                  {book.name}
-                </Table.Cell>
-                <Table.Cell>{book.totalWordCount}</Table.Cell>
-                <Modal
-                  closeIcon
-                  open={this.state.showRemoveBookModal}
-                  trigger={
-                    <Table.Cell>
-                      <Button
-                        color={"red"}
-                        onClick={() =>
-                          this.setState({ showRemoveBookModal: true })
-                        }
-                      >
-                        Remove
-                      </Button>
-                    </Table.Cell>
-                  }
-                  onClose={() => this.setState({ showRemoveBookModal: false })}
-                  onOpen={() => this.setState({ showRemoveBookModal: true })}
-                >
-                  <Header content="Confirm" />
-                  <Modal.Content>
-                    <p>Do you really want to delete this book: {book.name}?</p>
-                  </Modal.Content>
-                  <Modal.Actions>
-                    <Button
-                      color="red"
-                      onClick={() =>
-                        this.setState({ showRemoveBookModal: false })
-                      }
-                    >
-                      <Icon name="remove" /> No
-                    </Button>
-                    <Button
-                      color="green"
-                      onClick={() => this.handleRemoveBook(book.id)}
-                    >
-                      <Icon name="checkmark" /> Yes
-                    </Button>
-                  </Modal.Actions>
-                </Modal>
-                <Table.Cell>
-                  <Button>Review</Button>
-                </Table.Cell>
-              </Table.Row>
-            ))}
-          </Table.Body>
-        </Table>
-      </Container>
-    );
-  }
+  }, [initiated]);
 
-  private async init() {
-    await this.initBooks();
-    this.setState({
-      initiated: true,
-    });
-  }
+  const init = async () => {
+    await initBooks();
+    setInitiated(true);
+  };
 
-  private async initBooks() {
+  const initBooks = async () => {
     const books = await serviceProvider.bookService.getBooks();
-    this.setState({ books });
-  }
-
-  private handleCloseOnAddBookModal = (addedBook?: BookVO) => {
-    const books =
-      addedBook === undefined
-        ? this.state.books
-        : this.state.books.concat(addedBook);
-    this.setState({
-      books,
-      showAddBookModal: false,
-    });
+    setBooks(books);
   };
 
-  private handleClickOnBook = (book: BookVO) => {
-    this.props.history.push("/book/" + book.id);
+  const handleCloseOnAddBookModal = (addedBook?: BookVO) => {
+    setBooks(addedBook === undefined ? books : books.concat(addedBook));
+    setShowAddBookModal(false);
   };
 
-  private handleRemoveBook = async (bookId: number) => {
+  const handleClickOnBook = (book: BookVO) => {
+    Router.toBookPage(book.id);
+  };
+
+  const handleRemoveBook = async (bookId: number) => {
     await serviceProvider.bookService.removeBook(bookId);
-    const newBooks = this.state.books.filter(book => book.id != bookId);
-    this.setState({
-      showRemoveBookModal: false,
-      books: newBooks,
-    });
+    const newBooks = books.filter((book) => book.id != bookId);
+    setShowAddBookModal(false);
+    setBooks(newBooks);
   };
+
+  return initiated ? (
+    <Container>
+      <Grid columns={3}>
+        <Grid.Row>
+          <Grid.Column width={8}>
+            <Header>Books</Header>
+          </Grid.Column>
+          <Grid.Column textAlign={"right"} width={8}>
+            <AddBookModal
+              visible={showAddBookModal}
+              onOpen={() => setShowAddBookModal(true)}
+              onClose={handleCloseOnAddBookModal}
+              trigger={
+                <Button icon labelPosition={"left"}>
+                  <Icon name={"add"} /> Add
+                </Button>
+              }
+            />
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+      <Divider />
+      <Table basic="very" selectable>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>Name</Table.HeaderCell>
+            <Table.HeaderCell>Total Word Count</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {books.map((book) => (
+            <Table.Row key={book.id}>
+              <Table.Cell
+                onClick={() => handleClickOnBook(book)}
+                className={"hover-link"}
+              >
+                {book.name}
+              </Table.Cell>
+              <Table.Cell>{book.totalWordCount}</Table.Cell>
+              <Modal
+                closeIcon
+                open={showRemoveBookModal}
+                trigger={
+                  <Table.Cell>
+                    <Button
+                      color={"red"}
+                      onClick={() => setShowRemoveBookModal(true)}
+                    >
+                      Remove
+                    </Button>
+                  </Table.Cell>
+                }
+                onClose={() => setShowRemoveBookModal(false)}
+                onOpen={() => setShowRemoveBookModal(true)}
+              >
+                <Header content="Confirm" />
+                <Modal.Content>
+                  <p>Do you really want to delete this book: {book.name}?</p>
+                </Modal.Content>
+                <Modal.Actions>
+                  <Button
+                    color="red"
+                    onClick={() => setShowRemoveBookModal(false)}
+                  >
+                    <Icon name="remove" /> No
+                  </Button>
+                  <Button
+                    color="green"
+                    onClick={() => handleRemoveBook(book.id)}
+                  >
+                    <Icon name="checkmark" /> Yes
+                  </Button>
+                </Modal.Actions>
+              </Modal>
+              <Table.Cell>
+                <Button>Review</Button>
+              </Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table>
+    </Container>
+  ) : (
+    <></>
+  );
 }
