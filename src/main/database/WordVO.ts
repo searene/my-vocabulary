50c002db9fb824f6043562d82dc84adcc7c6dbb1
searene <mycityofsky@gmail.com>
2021-07-02 21:12:31 +0800

    add the database folder

---
+++ src/main/database/WordVO.ts
@@ -0,0 +1,19 @@
+import { WordStatus } from "../enum/WordStatus";
+import { WordContext } from "../domain/WordContext";
+
+export type WordVO = {
+
+  /**
+   * Word in the book.
+   */
+  word: string;
+
+  /**
+   * The original word of the word in the book.
+   */
+  originalWord: string;
+
+  contextList: WordContext[];
+
+  status: WordStatus;
+};
